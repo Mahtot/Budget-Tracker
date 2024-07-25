772704e79e8df27733714b37629b748ebf8a7bb2
[mahtot] <[mahtotgher13@gmail.com]>
2024-07-25 12:28:00 +0300

    Done with the structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,30 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
-      primary : '#243c5a', 
-      secondary : {
-        100: '#243c5a',
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      gridColumn: {
+        'span-2-4': '2 / 4',
+        'span-4-6': '4 / 6',
+      },
+      screens: {
+        300:'300px',
+        475: '475px',
+        237: '237px',
+        280: '280px',
+        344: '344px',
+        531: '531px',
+        627: '627px',
+        785: '785px',
+        992:'992px',
+        994: '994px',
+        1097: '1097px',
+        1157: '1157px',
+        1207:'1207px',
+        1309 : '1309px',
+        1320:'1320px'
       }
     },
   },
